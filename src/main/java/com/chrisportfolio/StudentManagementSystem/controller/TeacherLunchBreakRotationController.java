23331a4fc55f7cd2eb9ae20bc3320d271f623d8d
barroodc <chrisbarrood@gmail.com>
2022-08-31 23:30:30 -0400

    Finished controller package.

--- src/main/java/com/chrisportfolio/StudentManagementSystem/controller/TeacherLunchBreakRotationController.java
+++ src/main/java/com/chrisportfolio/StudentManagementSystem/controller/TeacherLunchBreakRotationController.java
@@ -1,4 +1,43 @@
 package com.chrisportfolio.StudentManagementSystem.controller;
 
+import com.chrisportfolio.StudentManagementSystem.model.TeacherLunchBreakRotation;
+import com.chrisportfolio.StudentManagementSystem.repository.TeacherLunchBreakRotationRepository;
+import com.chrisportfolio.StudentManagementSystem.service.TeacherLunchBreakRotationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class TeacherLunchBreakRotationController {
+
+    @Autowired
+    TeacherLunchBreakRotationService teacherLunchBreakRotationService;
+
+    @Autowired
+    TeacherLunchBreakRotationRepository teacherLunchBreakRotationRepository;
+
+    @RequestMapping(value = "teacherLunchBreakRotations", method = RequestMethod.POST)
+    public TeacherLunchBreakRotation createTeacherLunchBreakRotations(
+            @RequestBody TeacherLunchBreakRotation teacherLunchBreakRotation) {
+        return teacherLunchBreakRotationService.createTeacherLunchBreakRotation(teacherLunchBreakRotation);
+    }
+
+    @RequestMapping(value = "teacherLunchBreakRotations", method = RequestMethod.GET)
+    public List<TeacherLunchBreakRotation> readTeacherLunchBreakRotations() {
+        return teacherLunchBreakRotationService.getTeacherLunchBreakRotation();
+    }
+
+    @RequestMapping(value = "teacherLunchBreakRotations/{teacherLunchBreakRotationId}", method = RequestMethod.PUT)
+    public TeacherLunchBreakRotation updateTeacherLunchBreakRotations(
+            @PathVariable("teacherLunchBreakRotationId") Long id,
+            @RequestBody TeacherLunchBreakRotation teacherLunchBreakRotationDetails) {
+        return teacherLunchBreakRotationService.updateTeacherLunchBreakRotation(id, teacherLunchBreakRotationDetails);
+    }
+
+    @RequestMapping(value = "teacherLunchBreakRotations/{teacherLunchBreakRotationId}", method = RequestMethod.DELETE)
+    public void deleteTeacherLunchBreakRotations(@PathVariable(value = "teacherLunchBreakRotationId") Long id) {
+        teacherLunchBreakRotationService.deleteTeacherLunchBreakRotation(id);
+    }
 }
--- src/main/java/com/chrisportfolio/StudentManagementSystem/controller/TeacherViewAllGradesInCourseController.java
+++ src/main/java/com/chrisportfolio/StudentManagementSystem/controller/TeacherViewAllGradesInCourseController.java
@@ -1,4 +1,43 @@
 package com.chrisportfolio.StudentManagementSystem.controller;
 
+import com.chrisportfolio.StudentManagementSystem.model.TeacherViewAllGradesInCourse;
+import com.chrisportfolio.StudentManagementSystem.repository.TeacherViewAllGradesInCourseRepository;
+import com.chrisportfolio.StudentManagementSystem.service.TeacherViewAllGradesInCourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class TeacherViewAllGradesInCourseController {
+
+    @Autowired
+    TeacherViewAllGradesInCourseService teacherViewAllGradesInCourseService;
+
+    @Autowired
+    TeacherViewAllGradesInCourseRepository teacherViewAllGradesInCourseRepository;
+
+    @RequestMapping(value = "teacherViewAllGradesInCourse", method = RequestMethod.POST)
+    public TeacherViewAllGradesInCourse createTeacherViewAllGradesInCourse(
+            @RequestBody TeacherViewAllGradesInCourse teacherViewAllGradesInCourse) {
+        return teacherViewAllGradesInCourseService.createTeacherViewAllGradesInCourse(teacherViewAllGradesInCourse);
+    }
+
+    @RequestMapping(value = "teacherViewAllGradesInCourse", method = RequestMethod.GET)
+    public List<TeacherViewAllGradesInCourse> readTeacherViewAllGradesInCourse() {
+        return teacherViewAllGradesInCourseService.getTeacherViewAllGradesInCourse();
+    }
+
+    @RequestMapping(value = "teacherViewAllGradesInCourse/{teacherViewAllGradesInCourseId}", method = RequestMethod.PUT)
+    public TeacherViewAllGradesInCourse updateTeacherViewAllGradesInCourse(
+            @PathVariable(value = "teacherViewAllGradesInCourseId") Long id,
+            @RequestBody TeacherViewAllGradesInCourse teacherViewAllGradesInCourseDetails) {
+        return teacherViewAllGradesInCourseService.updateTeacherViewAllGradesInCourse(id, teacherViewAllGradesInCourseDetails);
+    }
+
+    @RequestMapping(value = "teacherViewAllGradesInCourse/{teacherViewAllGradesInCourseId}", method = RequestMethod.DELETE)
+    public void deleteTeacherViewAllGradesInCourse(@PathVariable(value = "teacherViewAllGradesInCourseId") Long id) {
+        teacherViewAllGradesInCourseService.deleteTeacherViewAllGradesInCourse(id);
+    }
 }
